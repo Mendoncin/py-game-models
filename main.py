f3d992bd56acc269b05910a3c431e6df56cf1388
Mendoncin <gabriel.cabecao.lol@gmail.com>
2026-04-28 00:25:59 -0300

    solution

--- main.py
+++ main.py
@@ -1,10 +1,60 @@
-import init_django_orm  # noqa: F401
+import init_django_orm  # noqa
+import json
 
 from db.models import Race, Skill, Player, Guild
 
 
 def main() -> None:
-    pass
+    with open("players.json", "r", encoding="utf-8") as f:
+        dados = json.load(f)
+
+    for name, data in dados.items():
+
+        race_data = data["race"]
+        race = Race.objects.filter(name=race_data["name"]).first()
+
+        if not race:
+            race = Race(
+                name=race_data["name"],
+                description=race_data.get("description", "")
+            )
+            race.save()
+
+        for skill_data in race_data.get("skills", []):
+            skill = Skill.objects.filter(name=skill_data["name"]).first()
+
+            if not skill:
+                skill = Skill(
+                    name=skill_data["name"],
+                    bonus=skill_data["bonus"],
+                    race=race
+                )
+                skill.save()
+
+        guild = None
+        guild_data = data.get("guild")
+
+        if guild_data:
+            guild = Guild.objects.filter(name=guild_data["name"]).first()
+
+            if not guild:
+                guild = Guild(
+                    name=guild_data["name"],
+                    description=guild_data.get("description")
+                )
+                guild.save()
+
+        player = Player.objects.filter(nickname=name).first()
+
+        if not player:
+            player = Player(
+                nickname=name,
+                email=data["email"],
+                bio=data["bio"],
+                race=race,
+                guild=guild
+            )
+            player.save()
 
 
 if __name__ == "__main__":
